6257d28d3955a61fc6531484bb4687c0e2e3b9e5
Danas <danas.mikelionis@gmail.com>
2019-05-09 22:35:32 +0300

    added default disabled row inputs, disabling inputs if item has no name

--- JS/script.js
+++ JS/script.js
@@ -2,30 +2,49 @@
     function () {
         var createItemDiv = document.getElementById("create-item");
         var newItemRowDivs = document.getElementsByClassName("item-row");
-        
+
         var inputsItemRowWhat = [];
         var inputsItemRowTimes = [];
         var inputsItemRowTimeSec = [];
-        
+
         var labelsItemRowWhat = [];
         var labelsItemRowTimes = [];
         var labelsItemRowTimeSec = [];
-        
+
         var addNewItemButton = document.getElementById("add-new-item");
-        
-        
+
+        var newItemNameInput = document.getElementById("new-item-name");
+
+
         setNewItemInputs();
         setNewItemLabels();
-        
-        addActionsToLastRow(1);
 
+        addActionsToLastRow(1);
+        
+        disableEnableAllRowInputs(true);
+
+        newItemNameInput.addEventListener("keyup", function () {
+            if (newItemNameInput.value === "") {
+                disableEnableAllRowInputs(true);
+            } else {
+                disableEnableAllRowInputs(false);
+            }
+        });
+
+        function disableEnableAllRowInputs(trueFalse) {
+            for (var i = 0; i < newItemRowDivs.length; i++) {
+                inputsItemRowWhat[i].disabled = trueFalse;
+                inputsItemRowTimes[i].disabled = trueFalse;
+                inputsItemRowTimeSec[i].disabled = trueFalse;
+            }
+        }
 
         function setNewItemInputs() {
             inputsItemRowWhat = document.getElementsByClassName("item-row-what");
             inputsItemRowTimes = document.getElementsByClassName("item-row-times");
             inputsItemRowTimeSec = document.getElementsByClassName("item-row-time-sec");
         }
-        
+
         function setNewItemLabels() {
             labelsItemRowWhat = document.querySelectorAll("label[for='item-row-what']");
             labelsItemRowTimes = document.querySelectorAll("label[for='item-row-times']");
@@ -37,7 +56,7 @@
 
                 var newItemRow = document.createElement("div");
                 newItemRow.classList.add("item-row");
-                
+
                 var input1 = document.createElement("input");
                 input1.type = "text";
                 input1.classList.add("item-row-what");
@@ -49,7 +68,7 @@
                 input3.type = "number";
                 input3.min = "0";
                 input3.classList.add("item-row-time-sec");
-                
+
                 var label1 = document.createElement("label");
                 label1.for = "item-row-what";
                 label1.innerHTML = "Full path in pc: ";
@@ -59,7 +78,7 @@
                 var label3 = document.createElement("label");
                 label3.for = "item-row-time-sec";
                 label3.innerHTML = "Time to play(s): ";
-                
+
 
                 newItemRow.appendChild(label1);
                 newItemRow.appendChild(input1);
@@ -69,12 +88,12 @@
                 newItemRow.appendChild(input3);
                 createItemDiv.appendChild(newItemRow);
                 createItemDiv.appendChild(addNewItemButton);
-                
+
                 setNewItemInputs();
                 setNewItemLabels();
                 newItemRowDivs = document.getElementsByClassName("item-row");
                 console.log(newItemRowDivs);
-                
+
                 addActionsToLastRow(getNewItemRowCount());
             }
         }
@@ -91,7 +110,7 @@
             inputsItemRowWhat[rowLength - 1].addEventListener("keyup", function () {
                 newItemRowChange();
             });
-            
+
             inputsItemRowTimes[rowLength - 1].addEventListener("keyup", function () {
                 if (inputsItemRowTimes[rowLength - 1].value != "") {
                     inputsItemRowTimeSec[rowLength - 1].value = "";
@@ -105,7 +124,7 @@
                     newItemRowChange();
                 }
             });
-            
+
         }
     }
 )();
